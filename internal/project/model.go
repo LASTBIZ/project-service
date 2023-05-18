7d57abd8fe76d486eed431a3eae5aa865aa4116f
1Suro1 <timu.zaxarov@gmail.com>
2023-05-18 23:44:12 +0900

    starting project-service

--- internal/project/model.go
+++ internal/project/model.go
@@ -2,22 +2,50 @@ package project
 
 import "time"
 
+type Image struct {
+	RoadMapImgUrl string
+	MainImageUrl  string
+}
+
+type Date struct {
+	StartDate time.Time
+	EndDate   time.Time
+}
+
+type Budget struct {
+	Current uint64
+	Need    uint64
+}
+
 type Project struct {
-	ID             uint64
+	ID             uint64 `gorm:"primaryKey"`
+	Images         Image  `gorm:"embedded"`
+	Dates          Date   `gorm:"embedded"`
+	Budgets        Budget `gorm:"embedded"`
 	Location       string
-	OrganizationID uint32
-	RoadMapImgUrl  string
 	Name           string
-	MainImage      string
-	StartDate      time.Time
-	EndDate        time.Time
+	Description    string `gorm:"type:text"`
 	Industry       string
-	Budget         uint64
-	EndBudget      uint64
+	Investors      []Investor
+	Roadmaps       []Roadmap
+	OrganizationID uint32
+}
+
+type Roadmap struct {
+	ID          uint64 `gorm:"primaryKey"`
+	Dates       Date   `gorm:"embedded"`
+	Name        string
+	Description string `gorm:"type:text"`
+	Job         string `gorm:"type:text"`
+	Target      string `gorm:"type:text"`
+	ProjectID   uint64
 }
 
 type Investor struct {
-	ID     uint64
-	Money  uint64
-	UserID uint32
+	ID             uint64 `gorm:"primaryKey"`
+	Money          uint64
+	FullName       string
+	UserID         uint32
+	OrganizationID uint32
+	ProjectID      uint64
 }
--- internal/project/storage.go
+++ internal/project/storage.go
@@ -1,1 +1,51 @@
 package project
+
+import "gorm.io/gorm"
+
+type Storage struct {
+	db *gorm.DB
+}
+
+func NewProjectStorage(db *gorm.DB) *Storage {
+	return &Storage{
+		db: db,
+	}
+}
+
+func (s Storage) CreateProject(project Project) error {
+	return s.db.Create(&project).Error
+}
+
+func (s Storage) UpdateProject(projectID uint32, project Project) error {
+	return s.db.Model(&Project{}).Where("id = ?", projectID).
+		Updates(Project{
+			Location:       project.Location,
+			Name:           project.Name,
+			Images:         project.Images,
+			Dates:          project.Dates,
+			Budgets:        project.Budgets,
+			Description:    project.Description,
+			Industry:       project.Industry,
+			Investors:      project.Investors,
+			Roadmaps:       project.Roadmaps,
+			OrganizationID: project.OrganizationID,
+		}).Error
+}
+
+func (s Storage) DeleteProject(projectID uint32) error {
+	return s.db.Where("id = ?", projectID).Delete(&Project{}).Error
+}
+
+func (s Storage) GetProjectByID(projectID uint32) (*Project, error) {
+	var getProject Project
+	err := s.db.Where("id = ?", projectID).First(&getProject).Error
+	return &getProject, err
+}
+
+// GetProjects
+// paginate example db.Offset(offset).Limit(pageSize)
+func (s Storage) GetProjects(paginate func(db *gorm.DB) *gorm.DB) (*[]Project, error) {
+	var getProjects []Project
+	err := s.db.Scopes(paginate).First(&getProjects).Error
+	return &getProjects, err
+}
