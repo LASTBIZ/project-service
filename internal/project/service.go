01b0a99b90d0746817d4fec793e636bcae35d420
1Suro1 <timu.zaxarov@gmail.com>
2023-05-19 09:57:25 +0900

    add proto

--- internal/project/service.go
+++ internal/project/service.go
@@ -1,1 +1,40 @@
 package project
+
+import (
+	"context"
+	"lastbiz/project-service/pkg/project"
+)
+
+type Service struct {
+	storage Storage
+	project.UnimplementedProjectServiceServer
+}
+
+func NewProjectService() project.ProjectServiceServer {
+	return &Service{}
+}
+
+func (s Service) CreateProject(ctx context.Context, request *project.CreateProjectRequest) (*project.CreateProjectResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) DeleteProject(ctx context.Context, request *project.DeleteProjectRequest) (*project.DeleteProjectResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) UpdateProject(ctx context.Context, request *project.UpdateProjectRequest) (*project.UpdateProjectResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetProjectByID(ctx context.Context, request *project.GetProjectByIdRequest) (*project.GetProjectByIdResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetProjects(ctx context.Context, request *project.GetProjectsRequest) (*project.GetProjectsResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
