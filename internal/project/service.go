ad213b4491d9606e42f63dd8760eaa106de30ba6
suro suro <bogayt@gmail.com>
2023-05-19 11:35:16 +0900

    edit

--- internal/project/service.go
+++ internal/project/service.go
@@ -15,26 +15,20 @@ func NewProjectService() project.ProjectServiceServer {
 }
 
 func (s Service) CreateProject(ctx context.Context, request *project.CreateProjectRequest) (*project.CreateProjectResponse, error) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (s Service) DeleteProject(ctx context.Context, request *project.DeleteProjectRequest) (*project.DeleteProjectResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
 }
 
 func (s Service) UpdateProject(ctx context.Context, request *project.UpdateProjectRequest) (*project.UpdateProjectResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
 }
 
 func (s Service) GetProjectByID(ctx context.Context, request *project.GetProjectByIdRequest) (*project.GetProjectByIdResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
 }
 
 func (s Service) GetProjects(ctx context.Context, request *project.GetProjectsRequest) (*project.GetProjectsResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
 }
